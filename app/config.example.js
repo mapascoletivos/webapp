93105d51a60ce6e3dfaa8e08436f20136106bc0d
Miguel Peixe <miguel@cardume.art.br>
2014-04-16 13:37:46 -0300

    change config example defaults

--- app/config.example.js
+++ app/config.example.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-	siteUrl: 'http://my-yby-CLIENT.com',
-	server: 'http://my-yby-SERVER.com',
+	siteUrl: 'http://localhost:8000', // Client URL. Eg: http://my-yby-service.com
+	server: 'http://localhost:3000', // Server URL. Eg: http://server.my-yby-service.com
 	apiPrefix: '/api/v1', // You probably won't change this
 	oauth: {
 		facebook: '', // Facebook Client ID
