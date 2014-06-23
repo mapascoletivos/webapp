4bea306f8d4606335aa8f2b3a7f86b6aebbdbdf0
Miguel Peixe <miguel@cardume.art.br>
2014-06-23 11:24:46 -0300

    change server config

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static(__dirname + '/public'));
+app.configure(function() {
+
+	app.use(require('prerender-node'));
+
+	app.use('/', express.static(__dirname + '/public'));
+
+});
 
 app.get('/*', function(req, res) {
 	res.sendfile('public/views/layouts/default.html');
 });
-
-app.use(require('prerender-node'));
 
 // Start the app by listening on <port>
 var port = process.env.PORT || 8000
