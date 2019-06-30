201f3f11bfab43e9daeec10abfdc206dc2bd6098
Vitor George <vitor.george@gmail.com>
2019-06-30 11:34:55 +0100

    Add envify plugin.

--- app/config.js
+++ app/config.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var env = process.env;
-
 module.exports = {
-	siteUrl: env.SITE_URL || 'http://localhost:8000', // Client URL. Eg: http://my-yby-service.com
-	server: env.SERVER_URL || 'http://localhost:3000', // Server URL. Eg: http://server.my-yby-service.com
-	apiPrefix: env.API_PREFIX || '/api/v1' // You probably won't change this
+	siteUrl: process.env.SITE_URL || 'http://localhost:8000',
+	server: process.env.SERVER_URL || 'http://localhost:3000',
+	apiPrefix: process.envAPI_PREFIX || '/api/v1'
 };
